62104bca6b85ba30271bf5c735ac81caa4dfd2f9
Teresa de Jesús <l17350318@ittux.edu.mx>
2020-11-17 18:15:07 -0600

    Agregado Express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo!!!");
+const express = require('express');
+
+// Crear servidor Express
+
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+console.log('Se recibió una petición en /');
+res.json({
+    ok: true,
+    msg: "Recibido"
+});
+});
+
+// Configuración del puerto
+
+app.listen(4000, () => {
+    console.log(`Backend corriendo en el puerto ${400}`);
+})
